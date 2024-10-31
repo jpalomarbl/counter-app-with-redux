f90a5b324b4fc868009d894064a9f9a157e4f8eb
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu>
2024-10-31 14:15:40 +0000

    Finished daughter component

--- src/app/daughter/daughter.component.ts
+++ src/app/daughter/daughter.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter } from '@angular/core';
+import { GranddaughterComponent } from '../granddaughter/granddaughter.component';
 
 @Component({
   selector: 'app-daughter',
   standalone: true,
-  imports: [],
+  imports: [GranddaughterComponent],
+  inputs: ['counter'],
+  outputs: ['changeCounter'],
   templateUrl: './daughter.component.html',
   styleUrl: './daughter.component.scss'
 })
 export class DaughterComponent {
+  counter!: number;
+  changeCounter = new EventEmitter<number>();
 
+  constructor() {}
+
+  duplicate(): void {
+    this.counter *= 2;
+    this.changeCounter.emit(this.counter);
+  }
+
+  resetGrandDaughter(newCounter: number): void {
+    this.counter = newCounter;
+    this.changeCounter.emit(this.counter);
+  }
 }
