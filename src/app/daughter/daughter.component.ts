2de7109c6fc3f98a0492931e85d796a92d6cb46a
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu>
2024-10-31 14:11:49 +0000

    Initial commit

---
+++ src/app/daughter/daughter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daughter',
+  standalone: true,
+  imports: [],
+  templateUrl: './daughter.component.html',
+  styleUrl: './daughter.component.scss'
+})
+export class DaughterComponent {
+
+}
---
+++ src/app/granddaughter/granddaughter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-granddaughter',
+  standalone: true,
+  imports: [],
+  templateUrl: './granddaughter.component.html',
+  styleUrl: './granddaughter.component.scss'
+})
+export class GranddaughterComponent {
+
+}
