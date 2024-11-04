382077abf3477cdf2b83caa2b8900d9551e502be
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-11-04 10:01:45 +0000

    Store configured and implemented in mother component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DaughterComponent } from './daughter/daughter.component';
+import { DaughterComponent } from './Counter/daughter/daughter.component';
+import { GranddaughterComponent } from './Counter/granddaughter/granddaughter.component';
+import { Store } from '@ngrx/store';
+import { decrement, increment } from "./Counter/counter.actions";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DaughterComponent],
+  imports: [
+    RouterOutlet,
+    DaughterComponent,
+    GranddaughterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'counter-app-without-redux';
-  counter: number;
+  counter!: number;
 
-  constructor() {
-    this.counter = 20;
+  constructor(private store: Store<{ counter: number }>) {
+    // this.counter = 20;
+
+    this.store.subscribe((state) => {
+      this.counter = state.counter;
+    });
   }
 
   increase(): void {
-    this.counter += 1;
+    // this.counter += 1;
+
+    this.store.dispatch(increment());
   }
 
   decrease(): void {
-    this.counter -= 1;
+    // this.counter -= 1;
+
+    this.store.dispatch(decrement());
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 
+import { counterReducer } from './Counter/counter.reducer';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore({ counter: counterReducer })]
 };
